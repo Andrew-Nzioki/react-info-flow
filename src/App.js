fe0e1d0942eaccf35f5632107e4fa9a34a180422
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-22 02:00:22 +0300

    exploring react data flow from child to parent up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import PetCard from "./components/PetCard";
+import { pets as petsArray } from "./components/data"
 
 function App() {
+  const [pets, setPets] = useState(petsArray)
+  
+  const petCards = pets.map((petObj) => {
+    return (
+      <PetCard
+        key={petObj.id}
+        name={petObj.name}
+        image={petObj.image}
+        favSnacks={petObj.favSnacks}
+        isAdopted={petObj.isAdopted}
+      />
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>{petCards}</main>
     </div>
   );
 }
